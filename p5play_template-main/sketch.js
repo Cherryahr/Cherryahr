dd5ed1b649f2dd7d818d1f742b2934cf51d8a497
Cherryahr <94198925+Cherryahr@users.noreply.github.com>
2021-11-12 17:47:12 -0300

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,30 @@
+var quadrado;
+
+function setup() {
+  createCanvas(400,400);
+  quadrado = createSprite(100,100,20,20);
+}
+
+function draw() 
+{
+  background(30);
+
+if(keyDown(LEFT_ARROW)){
+  quadrado.position.x = quadrado.position.x -5;
+}
+if(keyDown(RIGHT_ARROW)){
+  quadrado.position.x = quadrado.position.x +5;
+}
+if(keyDown(UP_ARROW)){
+  quadrado.position.y = quadrado.position.y -5;
+}
+if(keyDown(DOWN_ARROW)){
+  quadrado.position.y = quadrado.position.y +5;
+}
+  drawSprites();
+
+}
+
+
+
+
